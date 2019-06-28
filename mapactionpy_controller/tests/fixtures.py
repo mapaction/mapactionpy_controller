b43fa18eebc14dd6fe0c4d6352d587a7d68ab098
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-06-28 21:28:04 +0100

    bascis fixtures

---
+++ mapactionpy_controller/tests/fixtures.py
@@ -0,0 +1,32 @@
+
+
+
+fixture_datasource_query = """
+{
+"settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
+
+
+fixture_datasource_result_one_dataset_per_layer = """
+{
+"settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
+
+
+fixture_datasource_result_missing_layer = """
+{
+"settlement_points": "",
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
+
+fixture_datasource_result_one_dataset_per_layer = """
+{
+"settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
