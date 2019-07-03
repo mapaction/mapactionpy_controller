6c475dcc36cafc8c68e324b1768df97b4b40a1fb
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-07-03 22:54:09 +0100

    update fixtures and basic test

--- mapactionpy_controller/tests/fixtures.py
+++ mapactionpy_controller/tests/fixtures.py
@@ -1,32 +1,26 @@
 
+fixture_datasource_query = r"""
+{
+"settlement_points": "^XXX_stle_stl_pt_(.*?)_(.*?)_([phm][phm])(_(.+))",
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
 
-
-fixture_datasource_query = """
+fixture_datasource_result_one_dataset_per_layer = r"""
 {
 "settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
 "airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
 }
 """
 
-
-fixture_datasource_result_one_dataset_per_layer = """
-{
-"settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
-"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
-}
-"""
-
-
-fixture_datasource_result_missing_layer = """
+fixture_datasource_result_missing_layer = r"""
 {
 "settlement_points": "",
 "airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
 }
 """
 
-fixture_datasource_result_one_dataset_per_layer = """
-{
-"settlement_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\gbr_stle_stle_pt_s0_naturalearth_pp.shp",
-"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
-}
-"""
+fixture_regex_without_iso3_code = r'^XXX_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))'
+fixture_regex_with_iso3_code = r'^gbr_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))'
+fixture_regex_without_negative_iso3_code = r'^(?!(XXX))_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))'
+fixture_regex_with_negative_iso3_code = r'^(?!(gbr))_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))'
--- mapactionpy_controller/tests/test_mapactionpy_controller.py
+++ mapactionpy_controller/tests/test_mapactionpy_controller.py
@@ -1,10 +1,27 @@
 from unittest import TestCase
+import fixtures
 
 import mapactionpy_controller
 
 class TestMAController(TestCase):
     def test_alway_fail(self):
-        self.assertTrue(false)
+        self.assertTrue(False)
 		
     def test_alway_pass(self):
-        self.assertTrue(true)
+        self.assertTrue(True)
+
+    def test_substitute_iso3_in_regex(self):
+        iso3 = 'gbr'
+        test_str_positive = mapactionpy_controller.substitute_iso3_in_regex(
+            fixtures.fixture_regex_without_iso3_code, iso3)
+        self.assertEqual(test_str_positive, fixtures.fixture_regex_with_iso3_code)
+
+        test_str_negative = mapactionpy_controller.substitute_iso3_in_regex(
+            fixtures.fixture_regex_with_negative_iso3_code, iso3)
+        self.assertEqual(test_str_negative, fixtures.fixture_regex_with_negative_iso3_code)
+
+        self.assertTrue(False,
+                        "Test test_substitute_iso3_in_regex currently fail. The implenmentation"
+                        " does not need to include a function"
+                        " `mapactionpy_controller.substitute_iso3_in_regex`. This is included to"
+                        " be _indicative_ of the tests required.")
