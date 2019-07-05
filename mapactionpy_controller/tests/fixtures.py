4b0d45e3af77d52b6a3c7d2f90c6f6e3e3055881
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-07-05 16:24:06 +0100

    added test for searching for shapefiles

--- mapactionpy_controller/tests/fixtures.py
+++ mapactionpy_controller/tests/fixtures.py
@@ -2,6 +2,15 @@
 fixture_datasource_query = r"""
 {
 "settlement_points": "^XXX_stle_stl_pt_(.*?)_(.*?)_([phm][phm])(_(.+))",
+"surrounding_counties": "^(?!(XXX))_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))"
+"airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
+}
+"""
+
+fixture_datasource_intermediatory_query = r"""
+{
+"settlement_points": "^gbr_stle_stl_pt_(.*?)_(.*?)_([phm][phm])(_(.+))",
+"surrounding_counties": "^(?!(gbr))_admn_ad0_py_(.*?)_(.*?)_([phm][phm])(_(.+))"
 "airports_points": "D:\MapAction\2019-06-12-GBR\GIS\2_Active_Data\232_tran\scr_tran_air_pt_s1_ourairports_pp.shp"
 }
 """
--- mapactionpy_controller/tests/test_mapactionpy_controller.py
+++ mapactionpy_controller/tests/test_mapactionpy_controller.py
@@ -1,23 +1,27 @@
+import unittest
 from unittest import TestCase
 from . import fixtures
 
 import mapactionpy_controller
 
 class TestMAController(TestCase):
+
+    def setUp(self):
+        self.iso3 = 'gbr'
+
     def test_alway_fail(self):
         self.assertTrue(False)
-		
+
     def test_alway_pass(self):
         self.assertTrue(True)
 
     def test_substitute_iso3_in_regex(self):
-        iso3 = 'gbr'
         test_str_positive = mapactionpy_controller.substitute_iso3_in_regex(
-            fixtures.fixture_regex_without_iso3_code, iso3)
+            fixtures.fixture_regex_without_iso3_code, self.iso3)
         self.assertEqual(test_str_positive, fixtures.fixture_regex_with_iso3_code)
 
         test_str_negative = mapactionpy_controller.substitute_iso3_in_regex(
-            fixtures.fixture_regex_with_negative_iso3_code, iso3)
+            fixtures.fixture_regex_with_negative_iso3_code, self.iso3)
         self.assertEqual(test_str_negative, fixtures.fixture_regex_with_negative_iso3_code)
 
         self.assertTrue(False,
@@ -25,3 +29,22 @@ class TestMAController(TestCase):
                         " does not need to include a function"
                         " `mapactionpy_controller.substitute_iso3_in_regex`. This is included to"
                         " be _indicative_ of the tests required.")
+
+    def test_search_for_shapefiles(self):
+        # case where there is exactly one dataset per query
+        test_result_success = mapactionpy_controller.find_datasource(
+            fixtures.fixture_datasource_intermediatory_query)
+
+        self.assertEqual(test_result_success,
+                         fixtures.fixture_datasource_result_one_dataset_per_layer)
+
+        # case where there is a missing dataset for one or more query
+        test_result_success = mapactionpy_controller.find_datasource(
+            fixtures.fixture_datasource_intermediatory_query)
+
+        self.assertEqual(test_result_success,
+                         fixtures.fixture_datasource_result_missing_layer)
+
+
+if __name__ == '__main__':
+    unittest.main()
