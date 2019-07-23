923df8fd76806358f09540f45317268f869d7284
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-07-23 17:00:21 +0100

    fix reading csv to work in both py2.7 and py3.x

--- mapactionpy_controller/data_name_validators.py
+++ mapactionpy_controller/data_name_validators.py
@@ -1,5 +1,6 @@
 import csv
 import mapactionpy_controller as mac
+import six
 
 # abstract class
 # Done using the "old-school" methed described here, without using the abs module
@@ -28,22 +29,29 @@ class DataNameLookupClause(DataNameClause):
     def __init__(self, clause_name, csv_path, lookup_field):
         self.known_values = {}
 
-        with open(csv_path, 'rb') as csv_file:
-            self.filename = csv_path
-            csv_reader = csv.DictReader(
-                csv_file, delimiter=',', quotechar='"')
-            if lookup_field in csv_reader.fieldnames:
-                self.lookup_field = lookup_field
-            else:
-                raise mac.data_name_convention.DataNameException(
-                    'invalid validation lookup_field primary key {} in file {}'.format(lookup_field, csv_path))
+        if six.PY2:
+            with open(csv_path, 'rb') as csv_file:
+                self._init_known_values(csv_path, csv_file, lookup_field)
+        else:
+            with open(csv_path, 'r', newline='') as csv_file:
+                self._init_known_values(csv_path, csv_file, lookup_field)
 
-            for row in csv_reader:
-                pk = row[lookup_field]
-                non_lookup_keys = [x for x in row.keys() if x != lookup_field]
-                self.known_values[pk] = {n: row[n] for n in non_lookup_keys}
+    def _init_known_values(self, csv_path, csv_file, lookup_field):
+        self.filename = csv_path
+        csv_reader = csv.DictReader(
+            csv_file, delimiter=',', quotechar='"')
+        if lookup_field in csv_reader.fieldnames:
+            self.lookup_field = lookup_field
+        else:
+            raise mac.data_name_convention.DataNameException(
+                'invalid validation lookup_field primary key {} in file {}'.format(lookup_field, csv_path))
+
+        for row in csv_reader:
+            pk = row[lookup_field]
+            non_lookup_keys = [x for x in row.keys() if x != lookup_field]
+            self.known_values[pk] = {n: row[n] for n in non_lookup_keys}
 
 
     def validate(self, clause_value):
-        return self.known_values.has_key(clause_value)
+        return clause_value in self.known_values
 
