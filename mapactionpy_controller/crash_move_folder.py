8bd2be8634993bdcf1b167038db764f8de729881
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-09-06 10:17:02 +0100

    Layer name convention (#15)
    * naming convention to be case insensitive
    
    * extended data naming convention to layers and mxds
    
    * pep8 fixes
    
    * Check naming of layer and mxd files
    
    * validation of paths for CMF object
    
    * Consistant "Value" member of DataNameResults

--- mapactionpy_controller/crash_move_folder.py
+++ mapactionpy_controller/crash_move_folder.py
@@ -4,35 +4,48 @@ import os
 
 
 class CrashMoveFolder:
-    def __init__(self, cmf_path):
-        # cd = os.path.dirname(os.path.realpath(__file__))
-        # schema_path = os.path.join(cd, 'schemas', 'cmf-v0.1.schema')
-        # json_schema = open(schema_path, 'r').read()
-        # print(json_schema)
+    def __init__(self, cmf_path, verify_on_creation=True):
 
         self.path = os.path.dirname(cmf_path)
 
         with open(cmf_path, 'r') as f:
-            txt = f.read()
-            # print(txt)
-            # validate(txt, json_schema)
-            obj = json.loads(txt)
+            obj = json.loads(f.read())
 
             # Doubtless there is a more elegant way to do this.
-            self.event_description_file = os.path.join(
-                self.path, obj['event_description_file'])
-            self.original_data = os.path.join(
-                self.path, obj['original_data'])
+            self.event_description_file = os.path.join(self.path, obj['event_description_file'])
+            self.original_data = os.path.join(self.path, obj['original_data'])
             self.active_data = os.path.join(self.path, obj['active_data'])
-            self.layer_redering = os.path.join(
-                self.path, obj['layer_redering'])
-            self.mxd_templates = os.path.join(
-                self.path, obj['mxd_templates'])
-            self.qgis_templates = os.path.join(
-                self.path, obj['qgis_templates'])
+            self.layer_redering = os.path.join(self.path, obj['layer_redering'])
+            self.mxd_templates = os.path.join(self.path, obj['mxd_templates'])
+            self.mxd_products = os.path.join(self.path, obj['mxd_products'])
+            self.qgis_templates = os.path.join(self.path, obj['qgis_templates'])
             self.export_dir = os.path.join(self.path, obj['export_dir'])
-            self.dnc_definition = os.path.join(
-                self.path, obj['dnc_definition'])
+            self.dnc_definition = os.path.join(self.path, obj['dnc_definition'])
+            self.layer_nc_definition = os.path.join(self.path, obj['layer_nc_definition'])
+            self.mxd_nc_definition = os.path.join(self.path, obj['mxd_nc_definition'])
             self.default_jpeg_red_dpi = obj['default_jpeg_red_dpi']
             self.default_pdf_red_dpi = obj['default_pdf_red_dpi']
             self.default_emf_red_dpi = obj['default_emf_red_dpi']
+
+        if verify_on_creation and (not self.verify_paths()):
+            raise ValueError("Unable to verify existance of all files and directories defined in "
+                             "CrashMoveFolder {}".format(cmf_path))
+
+    def verify_paths(self):
+        results = (
+            # dirs
+            os.path.isdir(self.original_data),
+            os.path.isdir(self.active_data),
+            os.path.isdir(self.layer_redering),
+            os.path.isdir(self.mxd_templates),
+            os.path.isdir(self.mxd_products),
+            os.path.isdir(self.qgis_templates),
+            os.path.isdir(self.export_dir),
+            # files
+            os.path.exists(self.event_description_file),
+            os.path.exists(self.dnc_definition),
+            os.path.exists(self.layer_nc_definition),
+            os.path.exists(self.mxd_nc_definition)
+        )
+
+        return all(results)
