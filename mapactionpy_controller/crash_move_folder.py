0d6f4bbf80957aebc61103c8ae504df06aaa84df
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-11-17 16:00:43 +0000

    Controller integration (#34)
    * Include automation files
    
    * Add automation directory
    
    * event description is independent of CMF
    
    Also.  Changed default_donors_text to default_donor_credits
    
    * Remove unused automation directory from CrashMoveFolder class
    
    * Remove 'event_description_file'
    
    * Include automation files
    
    * Add automation directory
    
    * event description is independent of CMF
    
    Also.  Changed default_donors_text to default_donor_credits
    
    * Remove unused automation directory from CrashMoveFolder class
    
    * Remove 'event_description_file'
    
    * fix linting on crash_move_folder.pt
    
    * fix remaining linting on crash_move_folder.py
    
    * Added new fields to CMF description
    
    Added  `map_definitions`, `layer_properties`, `arcgis_version`
    Removed various DPI properties.
    Have not yet added the categories property.
    
    * Data Naming Convention path property is 'data_nc_convention'
    
    * fix failing unit tests
    
    * remove installs which are now handled by setup.py

--- mapactionpy_controller/crash_move_folder.py
+++ mapactionpy_controller/crash_move_folder.py
@@ -12,7 +12,7 @@ class CrashMoveFolder:
             obj = json.loads(f.read())
 
             # Doubtless there is a more elegant way to do this.
-            # 7 dirs
+            # 7 directories (alphabeltical order just for readablity)
             self.original_data = os.path.join(self.path, obj['original_data'])
             self.active_data = os.path.join(self.path, obj['active_data'])
             self.layer_rendering = os.path.join(self.path, obj['layer_rendering'])
@@ -20,42 +20,57 @@ class CrashMoveFolder:
             self.mxd_products = os.path.join(self.path, obj['mxd_products'])
             self.qgis_templates = os.path.join(self.path, obj['qgis_templates'])
             self.export_dir = os.path.join(self.path, obj['export_dir'])
-            # 4 files
-            self.event_description_file = os.path.join(self.path, obj['event_description_file'])
+            # 5 files (alphabeltical order just for readablity)
             self.data_nc_definition = os.path.join(self.path, obj['dnc_definition'])
             self.layer_nc_definition = os.path.join(self.path, obj['layer_nc_definition'])
+            self.layer_properties = os.path.join(self.path, obj['layer_properties'])
+            self.map_definitions = os.path.join(self.path, obj['map_definitions'])
             self.mxd_nc_definition = os.path.join(self.path, obj['mxd_nc_definition'])
-            # other values
-            self.default_jpeg_red_dpi = obj['default_jpeg_red_dpi']
-            self.default_pdf_red_dpi = obj['default_pdf_red_dpi']
-            self.default_emf_red_dpi = obj['default_emf_red_dpi']
+            # others
+            self.arcgis_version = obj['arcgis_version']
+            # self.categories = obj['categories']
 
         # paths_checked = self._verify_paths()
         if verify_on_creation and (not self.verify_paths()):
-            failing_paths = [path for path, valid in self._verify_paths().items() if not valid]
+            failing_paths = [path for path, valid in self._get_path_verification_as_dict().items() if not valid]
             failing_paths_str = "\n\t".join(failing_paths)
             raise ValueError("Unable to verify existence of all files and directories defined in "
                              "CrashMoveFolder '{}'. The values for these parameters could not be located:\n\t"
                              "{}".format(cmf_path, failing_paths_str))
 
-    def _verify_paths(self):
+    def _get_path_verification_as_dict(self):
         results = {}
 
-        # 7 dirs
-        results['original_data'] = os.path.isdir(self.original_data)
+        # 7 dirs (alphabeltical order just for readablity)
         results['active_data'] = os.path.isdir(self.active_data)
-        results['active_data'] = os.path.isdir(self.active_data)
-        results['mxd_templates'] = os.path.isdir(self.mxd_templates)
-        results['mxd_products'] = os.path.isdir(self.mxd_products)
-        results['qgis_templates'] = os.path.isdir(self.qgis_templates)
         results['export_dir'] = os.path.isdir(self.export_dir)
-        # 4 files
-        results['event_description_file'] = os.path.exists(self.event_description_file)
+        results['layer_rendering'] = os.path.isdir(self.layer_rendering)
+        results['mxd_products'] = os.path.isdir(self.mxd_products)
+        results['mxd_templates'] = os.path.isdir(self.mxd_templates)
+        results['original_data'] = os.path.isdir(self.original_data)
+        results['qgis_templates'] = os.path.isdir(self.qgis_templates)
+        # 5 files (alphabeltical order just for readablity)
         results['data_nc_definition'] = os.path.exists(self.data_nc_definition)
         results['layer_nc_definition'] = os.path.exists(self.layer_nc_definition)
+        results['layer_properties'] = os.path.exists(self.layer_properties)
+        results['map_definitions'] = os.path.exists(self.map_definitions)
         results['mxd_nc_definition'] = os.path.exists(self.mxd_nc_definition)
 
         return results
 
     def verify_paths(self):
-        return all(self._verify_paths().values())
+        # return all(all(_verify_paths().values()), self.verify_mxds())
+        return all(self._get_path_verification_as_dict().values())
+
+    def verify_mxds(self):
+        result = True
+        for category in (self.categories):
+            for orientation in ['landscape', 'portrait']:
+                templateFileName = self.arcgis_version + "_" + category + "_" + orientation
+
+                if (category == "reference"):
+                    templateFileName = templateFileName + "_bottom"
+                templateFileName = templateFileName + ".mxd"
+                if not os.path.exists(self.mxd_templates):
+                    result = False
+        return result
