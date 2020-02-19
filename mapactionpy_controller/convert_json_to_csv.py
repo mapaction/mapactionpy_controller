4e4a8c3a511a9233061b32b06dfcab26a06cf12b
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-02-19 00:01:36 +0000

    pep8 fixes

--- mapactionpy_controller/convert_json_to_csv.py
+++ mapactionpy_controller/convert_json_to_csv.py
@@ -1,51 +1,47 @@
 import os
-import sys
 import pandas as pd
 import json
 
 
-##
-# 
 def parse_directory(sourceDir, extList):
     sourceList = []
     for root, dirs, files in os.walk(sourceDir):
         for file in files:
             for ext in extList:
                 if file.endswith(ext):
-                    #print(os.path.join(root, file))
-                    #sourceList.append([root, file])
+                    # print(os.path.join(root, file))
+                    # sourceList.append([root, file])
                     sourceList.append(os.path.join(root, file))
     return sourceList
 
-##
-#
+
 def process_json_report(json_source):
     # Load JSON into Dictionary Type object
     # Note the JSON headers don't follow order of the file
-    with open(json_source) as json_data: 
+    with open(json_source) as json_data:
         json_d = json.load(json_data)
 
     # Various structure definitions - obtain from the Metis codebase
     # To ensure final column order
     prod_details = [
-            'classification',
-            'productName',
-            'result',
-            'summary',
-            ]
+        'classification',
+        'productName',
+        'result',
+        'summary',
+    ]
 
     correct_order = [
-            'classification',
-            'productName',
-            'result',
-            'added',            # Unpacked from results
-            'dataSource',       # Unpacked from results
-            'dateStamp',        # Unpacked from results
-            'hash',             # Unpacked from results
-            'layerName',        # Unpacked from results   
-            'message',          # Unpacked from results   
-            'summary',
-            ]
+        'classification',
+        'productName',
+        'result',
+        'added',            # Unpacked from results
+        'dataSource',       # Unpacked from results
+        'dateStamp',        # Unpacked from results
+        'hash',             # Unpacked from results
+        'layerName',        # Unpacked from results
+        'message',          # Unpacked from results
+        'summary',
+    ]
 
     # Add to a Pandas Data frame - or process into seprate tables.
     df_start = pd.DataFrame.from_dict(json_d)
@@ -63,13 +59,13 @@ def process_json_report(json_source):
     return df_final
 
 
-# 
+#
 def main(arvg=None):
     # Define location of folder with JSON report files.
     # To do - set up argument parsing...
-    root = r'J:\04_MapAction\Projects\Metis\TaskManager'        
-    json_dir = r'json_examples'                                
-    json_dir = os.path.join(root, json_dir)                   
+    root = r'J:\04_MapAction\Projects\Metis\TaskManager'
+    json_dir = r'json_examples'
+    json_dir = os.path.join(root, json_dir)
     # Get all reports as a list of file paths
     reports = parse_directory(json_dir, ['.json'])
     # Loop through the folder of JSON reports
@@ -90,5 +86,3 @@ def main(arvg=None):
 
 if __name__ == '__main__':
     main()
-
-
