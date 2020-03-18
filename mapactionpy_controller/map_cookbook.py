8875cdf0262f66a3383f0c8c5bc2f60be4834e01
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-03-18 14:27:02 +0000

    Bring files un changed across from controller

---
+++ mapactionpy_controller/map_cookbook.py
@@ -0,0 +1,34 @@
+# TODO: asmith 2020/03/04
+# This whole file should be moved to the mapactionpy_controller module.
+from map_recipe import MapRecipe
+import json
+
+
+class MapCookbook:
+    """
+    MapCookbook - Contains recipes for Map Products
+    """
+
+    def __init__(self, cookbookJsonFile):
+        """
+        Sets path for Map Cookbook json file.
+        Creates empty list of of products.
+
+        Arguments:
+           cookbookJsonFile {str} -- path to Map Cookbook json file "mapCookbook.json"
+        """
+        # @TODO Add validation +
+        # pass in LayerProperties object, and validate
+        self.cookbookJsonFile = cookbookJsonFile
+        self.products = {}
+        self._parse()
+
+    def _parse(self):
+        """
+        Reads product "recipes" from Map Cookbook json file
+        """
+        with open(self.cookbookJsonFile) as json_file:
+            jsonContents = json.load(json_file)
+            for recipe in jsonContents['recipes']:
+                rec = MapRecipe(recipe)
+                self.products[recipe['product']] = rec
---
+++ mapactionpy_controller/map_layer.py
@@ -0,0 +1,21 @@
+# TODO: asmith 2020/03/04
+# This whole file should be moved to the mapactionpy_controller module.
+from label_class import LabelClass
+
+
+class MapLayer:
+    def __init__(self, row):
+        """Constructor.  Creates an instance of layer properties
+
+        Arguments:
+            row {dict} -- From the layerProperties.json file
+        """
+        self.mapFrame = row["MapFrame"]
+        self.layerName = row["LayerName"]
+        self.regExp = row["RegExp"]
+        self.definitionQuery = row["DefinitionQuery"]
+        self.display = row["Display"]
+        self.addToLegend = row["AddToLegend"]
+        self.labelClasses = list()
+        for labelClass in row["LabelClasses"]:
+            self.labelClasses.append(LabelClass(labelClass))
