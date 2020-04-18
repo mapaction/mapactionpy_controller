5bc4cf72ab657038f2a949a073734280398d116a
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-04-18 20:47:31 +0100

    test for mismatch between lyrs in LayerProps and Cookbook

--- mapactionpy_controller/tests/test_map_cookbook.py
+++ mapactionpy_controller/tests/test_map_cookbook.py
@@ -53,4 +53,41 @@ class TestMapCookBook(TestCase):
         self.assertIsInstance(test_mcb, MapCookbook)
 
     def test_layer_props_and_cookbook_mismatch(self):
-        self.fail()
+        cmf = CrashMoveFolder(self.path_to_valid_cmf_des)
+        cmf.layer_properties = os.path.join(
+            self.parent_dir, 'tests', 'testfiles', 'fixture_layer_properties_four_layers.json'
+        )
+        cmf.map_definitions = os.path.join(
+            self.parent_dir, 'tests', 'testfiles', 'fixture_cookbook_1map_4layers.json'
+        )
+        test_lp = LayerProperties(cmf, "test", verify_on_creation=False)
+
+        # 1) map_cb and lp match
+        test_mcb = MapCookbook(cmf, test_lp, verify_on_creation=True)
+        self.assertIsInstance(test_mcb, MapCookbook)
+
+        # 2) layer in map_cb but not in lp
+        # 3) layer in lp but not in map_cb
+        # 4) unmatched layers in both lp and map_cb
+        testcases = (
+            ('fixture_cookbook_1map_5layers.json', ("mainmap-tran-rds-ln-s0-allmaps")),
+            ('fixture_cookbook_1map_3layers.json', ("locationmap_stle_stl_pt_s0_locationmaps")),
+            ('fixture_cookbook_1map_mismatch_layers.json',
+                ("mainmap-tran-rds-ln-s0-allmaps", "locationmap_stle_stl_pt_s0_locationmaps")
+             ),
+        )
+
+        for cb_file, strings_in_ex_msg in testcases:
+            cmf.map_definitions = os.path.join(self.parent_dir, 'tests', 'testfiles', cb_file)
+
+            with self.assertRaises(ValueError) as ve:
+                MapCookbook(cmf, test_lp, verify_on_creation=True)
+
+            if six.PY2:
+                self.assertRegexpMatches(str(ve.exception), "One or more layer names occur in only one of these files")
+                for s in strings_in_ex_msg:
+                    self.assertRegexpMatches(str(ve.exception), s)
+            else:
+                self.assertRegex(str(ve.exception), "One or more layer names occur in only one of these files")
+                for s in strings_in_ex_msg:
+                    self.assertRegex(str(ve.exception), s)
