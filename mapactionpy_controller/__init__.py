8d5190f3b14b55a91984a893105008f96dd964ef
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-04-24 11:15:34 +0100

    refactor

---
+++ mapactionpy_controller/__init__.py
@@ -0,0 +1,17 @@
+import json
+from jsonschema import validate
+from os import path
+
+CONFIG_SCHEMAS_DIR = path.join(path.abspath(path.dirname(__file__)), 'schemas')
+
+
+def _get_validator_for_schema(schema_file):
+
+    schema_path = path.join(CONFIG_SCHEMAS_DIR, schema_file)
+    with open(schema_path) as sf:
+        schema = json.load(sf)
+
+    def validate_against_schema(data):
+        validate(data, schema)
+
+    return validate_against_schema
--- mapactionpy_controller/map_layer.py
+++ mapactionpy_controller/map_layer.py
@@ -1,21 +1,7 @@
 from mapactionpy_controller.label_class import LabelClass
-import json
-from jsonschema import validate
-from os import path
+from mapactionpy_controller import _get_validator_for_schema
 
-
-def _get_schema():
-    root_dir = path.abspath(path.dirname(__file__))
-    schema_file = path.join(root_dir, 'schemas', 'layer_properties-v0.1.schema')
-    with open(schema_file) as sf:
-        return json.load(sf)
-
-
-SCHEMA = _get_schema()
-
-
-def validate_json(recipe_def):
-    validate(recipe_def, SCHEMA)
+validate_against_schema = _get_validator_for_schema('layer_properties-v0.1.schema')
 
 
 class MapLayer:
@@ -25,7 +11,7 @@ class MapLayer:
         Arguments:
             row {dict} -- From the layerProperties.json file
         """
-        validate_json(layer_def)
+        validate_against_schema(layer_def)
 
         self.mapFrame = layer_def["MapFrame"]
         self.layerName = layer_def["LayerName"]
