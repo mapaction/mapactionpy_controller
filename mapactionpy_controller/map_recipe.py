8540a9e9ee80fef85e8acb15ecda94d95cb15376
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-06-09 20:29:05 +0100

    reduce the length of map_recipe.py

--- mapactionpy_controller/map_recipe.py
+++ mapactionpy_controller/map_recipe.py
@@ -1,10 +1,10 @@
 import json
 from mapactionpy_controller.label_class import LabelClass
+from mapactionpy_controller.recipe_atlas import RecipeAtlas
 from mapactionpy_controller import _get_validator_for_config_schema
 import mapactionpy_controller.data_schemas as data_schemas
 from os import path
 
-validate_against_atlas_schema = _get_validator_for_config_schema('atlas-v0.2.schema')
 validate_against_layer_schema = _get_validator_for_config_schema('layer_properties-v0.2.schema')
 validate_against_recipe_schema = _get_validator_for_config_schema('map-recipe-v0.2.schema')
 
@@ -181,49 +181,6 @@ class RecipeFrame:
 
     def __setstate__(self, state):
         set_state_optional_fields(self, state, RecipeFrame.OPTIONAL_FIELDS)
-
-
-class RecipeAtlas:
-    def __init__(self, atlas_def, recipe, lyr_props):
-        validate_against_atlas_schema(atlas_def)
-
-        # Required fields
-        self.map_frame = atlas_def["map_frame"]
-        self.layer_name = atlas_def["layer_name"]
-        self.column_name = atlas_def["column_name"]
-
-        # Compare the atlas definition with the other parts of the recipe definition
-        if recipe.contains_frame(self.map_frame):
-            m_frame = recipe.get_frame(self.map_frame)
-        else:
-            raise ValueError(
-                'The Map Recipe definition is invalid. The "atlas" section refers to a map_frame '
-                ' ({}) that does not exist in the "map_frames" section of the recipe.'.format(
-                    self.map_frame)
-            )
-
-        if m_frame.contains_layer(self.layer_name):
-            lyr = m_frame.get_layer(self.layer_name)
-        else:
-            raise ValueError(
-                'The Map Recipe definition is invalid. The "atlas" section refers to a layer_name '
-                ' ({}) that does not exist in the relevant "map_frame" ({}) section of the recipe.'
-                ''.format(self.layer_name, self.map_frame)
-            )
-
-        if self.column_name not in lyr.data_schema['required']:
-            raise ValueError(
-                'The Map Recipe definition is invalid. The "atlas" section refers to a column_name '
-                ' ({}) that does not exist in the schema of the relevant layer ({}).'
-                ''.format(self.column_name, lyr.name)
-            )
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        """Overrides the default implementation (unnecessary in Python 3)"""
-        return not self.__eq__(other)
 
 
 class MapRecipe:
