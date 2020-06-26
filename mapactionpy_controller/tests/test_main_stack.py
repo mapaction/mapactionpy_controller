57a0bf22468c41ed19b67203763b5b706509f4d4
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-06-26 10:33:56 +0100

    tests with different types of state object

--- mapactionpy_controller/tests/test_main_stack.py
+++ mapactionpy_controller/tests/test_main_stack.py
@@ -5,20 +5,36 @@ from mapactionpy_controller.steps import Step
 
 class TestMainStack(TestCase):
 
-    def get_append_to_list(self, step_name):
+    def get_append_state(self, step_name, state_obj_type):
         def append_to_list(**kwargs):
             state = kwargs['state']
             state.append(step_name)
             return state
 
-        return append_to_list
+        def append_to_dict(**kwargs):
+            state = kwargs['state']
+            state[step_name] = step_name
+            return state
 
-    def build_example_steps(self, str_list):
+        def append_to_string(**kwargs):
+            state = kwargs['state']
+            state = state + step_name
+            return state
+
+        func_dict = {
+            'list': append_to_list,
+            'dict': append_to_dict,
+            'string': append_to_string
+        }
+
+        return func_dict[state_obj_type]
+
+    def build_example_steps(self, str_list, state_obj_type):
         step_list = []
         for step_name in str_list:
             step_list.append(
                 Step(
-                    self.get_append_to_list(step_name),
+                    self.get_append_state(step_name, state_obj_type),
                     'DEMO: Doing  {}'.format(step_name),
                     'DEMO: Passed {}'.format(step_name),
                     'DEMO: Failed {}'.format(step_name)
@@ -31,49 +47,64 @@ class TestMainStack(TestCase):
         pass
 
     def test_simple_step_list(self):
-        starter_list = ['AAA', 'BBB', 'CCC', 'DDD']
-        starter_steps = self.build_example_steps(starter_list)
-        final_state = process_stack(starter_steps, [])
 
-        self.assertEqual(starter_list, final_state)
-        print('\nfinal state = {}'.format(final_state))
+        starter_list = ['AAA', 'BBB', 'CCC', 'DDD']
+
+        test_cases = [
+            ('list', [], ['AAA', 'BBB', 'CCC', 'DDD']),
+            ('dict', {}, {'AAA': 'AAA', 'BBB': 'BBB', 'CCC': 'CCC', 'DDD': 'DDD'}),
+            ('string', '', 'AAABBBCCCDDD')
+        ]
+
+        for state_type, initial_state, expected_state in test_cases:
+            starter_steps = self.build_example_steps(starter_list, state_type)
+            final_state = process_stack(starter_steps, initial_state)
+            self.assertEqual(expected_state, final_state)
+            print('\nfinal state = {}'.format(final_state))
 
     def test_add_single_step_into_stack(self):
 
-        def get_inject_steps(**kwargs):
-            return self.build_example_steps(['111'])[0]
-
         starter_list = ['AAA', 'BBB', 'CCC', 'DDD']
-        expected_list = ['AAA', 'BBB', '111', 'DDD']
 
-        starter_steps = self.build_example_steps(starter_list)
+        test_cases = [
+            ('list', [], ['AAA', 'BBB', '111', 'DDD']),
+            ('dict', {}, {'AAA': 'AAA', 'BBB': 'BBB', '111': '111', 'DDD': 'DDD'}),
+            ('string', '', 'AAABBB111DDD')
+        ]
 
-        # change the func on one of the step, to a function which returns more Step objects
-        starter_steps[2].func = get_inject_steps
+        for state_type, initial_state, expected_state in test_cases:
+            def get_inject_steps(**kwargs):
+                return self.build_example_steps(['111'], state_type)[0]
 
-        final_state = process_stack(starter_steps, [])
-        self.assertEqual(final_state, expected_list)
-        # print('\nfinal list = {}'.format(final_state))
+            starter_steps = self.build_example_steps(starter_list, state_type)
+            # change the func on one of the step, to a function which returns more Step objects
+            starter_steps[2].func = get_inject_steps
+            final_state = process_stack(starter_steps, initial_state)
+            self.assertEqual(expected_state, final_state)
+            print('\nfinal list = {}'.format(final_state))
 
     def test_add_multiple_steps_into_stack(self):
 
-        def get_inject_steps(**kwargs):
-            return self.build_example_steps(['111', '222', '333'])
-
         starter_list = ['AAA', 'BBB', 'CCC', 'DDD']
-        expected_list = ['AAA', 'BBB', '111', '222', '333', 'DDD']
 
-        starter_steps = self.build_example_steps(starter_list)
+        test_cases = [
+            ('list', [], ['AAA', 'BBB', '111', '222', '333', 'DDD']),
+            ('dict', {}, {'AAA': 'AAA', 'BBB': 'BBB', '111': '111', '222':'222', '333':'333','DDD': 'DDD'}),
+            ('string', '', 'AAABBB111222333DDD')
+        ]
 
-        # change the func on one of the step, to a function which returns more Step objects
-        starter_steps[2].func = get_inject_steps
+        for state_type, initial_state, expected_state in test_cases:
+            def get_inject_steps(**kwargs):
+                return self.build_example_steps(['111', '222', '333'], state_type)
 
-        final_state = process_stack(starter_steps, [])
-        self.assertEqual(final_state, expected_list)
-        # print('\nfinal list = {}'.format(final_state))
+            starter_steps = self.build_example_steps(starter_list, state_type)
+            # change the func on one of the step, to a function which returns more Step objects
+            starter_steps[2].func = get_inject_steps
+            final_state = process_stack(starter_steps, initial_state)
+            self.assertEqual(expected_state, final_state)
+            print('\nfinal list = {}'.format(final_state))
 
-    def test_non_list_state_object(self):
-        pass
+
 
     # def test_distinguish_between_falsy_kwargs(self):
     #     inputs = [
