c1a3bd34cd9a52ca9e884d0c019891ddfa63fb23
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-10-14 22:22:51 +0100

    added get_template_by_aspect_ratio method

--- mapactionpy_controller/tests/test_plugin_base.py
+++ mapactionpy_controller/tests/test_plugin_base.py
@@ -27,13 +27,38 @@ class DummyRunner(BaseRunnerPlugin):
 
 class TestPluginBase(TestCase):
     def setUp(self):
-        pass
+        self.dummy_runner = DummyRunner()
 
     def test_get_all_templates_by_regex(self):
         pass
 
     def test_get_templates(self):
         pass
+
+    def test_get_template_by_aspect_ratio(self):
+        template_aspect_ratios = [
+            ('one',   1.1),
+            ('two',   2),
+            ('three', 2.32),
+            ('four',  3.1415),
+            ('five',  5),
+            ('six',   10)
+        ]
+
+        test_aspect_ratios = [
+            (5.0, 'five'),
+            (4.9, 'five'),
+            (5.1, 'five'),
+            (100, 'six'),
+            (0.0001, 'one'),
+            (7.5, 'six'),
+            (7.49998, 'five')
+        ]
+
+        for target_ar, expect_result in test_aspect_ratios:
+            actual_result = self.dummy_runner._get_template_by_aspect_ratio(template_aspect_ratios, target_ar)
+            print(target_ar, expect_result, actual_result)
+            self.assertEqual(expect_result, actual_result)
 
     def test_get_next_map_version_number(self):
         pass
