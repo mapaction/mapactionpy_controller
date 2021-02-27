d741dd9066b1d04070c6fa020b2d19f76fcce8f4
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2021-02-27 10:32:57 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ def readme():
 def _get_version_number():
     travis_build = environ.get('TRAVIS_BUILD_NUMBER')
     travis_tag = environ.get('TRAVIS_TAG')
-    repo_slug =  environ.get('TRAVIS_REPO_SLUG')
+    repo_slug = environ.get('TRAVIS_REPO_SLUG')
     if repo_slug:
-        repo_slug =  repo_slug.lower().strip()
+        repo_slug = repo_slug.lower().strip()
 
     if travis_build and (repo_slug == 'mapaction/mapactionpy_controller'):
         if travis_tag:
